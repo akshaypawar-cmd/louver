f94cc39cb53be0ad64e35e92425ea7e442f78f7d
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2025-12-12 15:45:05 +0530

    Add card-section

---
+++ src/Components/About/Cards.tsx
@@ -0,0 +1,114 @@
+import { type FC } from "react";
+import swimming from "@assets/swim_img.jpg";
+import batman from "@assets/batman_img.jpg";
+import { ArrowUpRight } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
+import { ArrowRight } from "lucide-react";
+
+const Cards: FC = () => {
+  return (
+    <>
+      <section className="max-w-7xl p-4 md:p-8 mx-auto">
+        <div className="flex flex-col md:flex-row sm:ml-28 md:ml-0 mx-auto gap-2">
+          <div className=" md:w-1/3 md:mt-3 text-left">
+            <button className="mb-6 border text-black  px-2 py-2 font-medium rounded-full text-xs">
+              About Louvre
+            </button>
+            <h2 className="text-xl md:text-xl font-normal leading-tight text-gray-800">
+              At Louvre sport <br className="block md:hidden" />
+              center, we're passionate about providing a
+              <br className="block md:hidden" /> top-notch environment for
+              <br /> athletes of all levels.
+            </h2>
+
+            <div className="mt-8">
+              <button className="inline-flex items-center gap-1 bg-gray-900 text-white px-4 py-2 rounded-full md:text-1xl shadow hover:opacity-95 cursor-pointer">
+                Get in Touch
+                <ArrowUpRight />
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full flex flex-col md:flex-row mt-4 gap-6">
+            <div className="relative flex-1 max-w-md rounded-2xl shadow-lg overflow-hidden">
+              <img
+                src={swimming}
+                alt="swimmer"
+                className="w-full h-64 md:h-80 lg:h-72 object-cover"
+              />
+
+              <button className="bg-black/70 text-white text-xs absolute top-4 left-4 text-2lg px-5 py-2 rounded-full  flex items-center gap-2  font-normal ">
+                Indoor Area
+              </button>
+
+              <div className="absolute bottom-8 left-4 right-4 flex items-center justify-between">
+                <div className="text-white">
+                  <h3 className="text-lg font-semibold drop-shadow">
+                    Versatile space for a wide
+                  </h3>
+                  <p className="text-sm drop-shadow  opacity-90">
+                    rand of activities
+                  </p>
+                </div>
+
+                <button className="bg-white/90 rounded-full w-10 h-10 flex items-center justify-center shadow-md">
+                  <ArrowUpRight />
+                </button>
+              </div>
+            </div>
+
+            <div className="relative flex-1 max-w-md bg-white rounded-2xl shadow-lg overflow-hidden">
+              <img
+                src={batman}
+                alt="basketball"
+                className="w-full h-59  object-cover"
+              />
+
+              <button className="bg-black/70 text-white text-xs absolute top-4 left-4 px-5 py-2 rounded-full font-normal">
+                Outdoor Area
+              </button>
+
+              <div className="absolute   bottom-21 md:bottom-24 p-2 left-4 right-4 flex items-center justify-between">
+                <div className="text-white">
+                  <h3 className="text-sm font-semibold drop-shadow">
+                    Futsal Court
+                  </h3>
+                  <p className="text-sm drop-shadow opacity-90">
+                    Play, train and compete
+                  </p>
+                </div>
+
+                <button className="bg-black text-white font-semibold rounded-full w-10 h-10 flex items-center justify-center shadow-md">
+                  <ArrowUpRight className="w-5 h-5" />
+                </button>
+              </div>
+              <div className="flex md:flex-row  items-center p-2 justify-between w-full gap-4">
+                <p className=" text-xs md:text-xs  text-gray-500 max-w-xs md:max-w-md">
+                  Discover your new - favorite <br className="lock " /> place to
+                  play, train, and compete.
+                </p>
+
+                <div className="flex items-center gap-2">
+                  <button
+                    aria-label="previous"
+                    className="w-9 h-9  font-light cursor-pointer rounded-full border border-gray-300 flex items-center justify-center"
+                  >
+                    <ArrowLeft className="h-4 w-4 text-gray-600 " />
+                  </button>
+                  <button
+                    aria-label="next"
+                    className="w-9 h-9 cursor-pointer rounded-full border border-gray-300 flex items-center justify-center"
+                  >
+                    <ArrowRight className=" w-4 h-4 text-gray-600 " />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Cards;
