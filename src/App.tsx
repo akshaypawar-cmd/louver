749f9e693299489f10eefb36e9fbeeed67dcf646
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2025-12-17 16:49:31 +0530

    all issues resoved

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import type { FC } from "react";
 import HeroSection from "./Components/Hero/HeroSection.js";
 import Cards from "./Components/About/Cards.js";
-
-import Facility from "./Components/Facility/Facility.js";
+import Facility from "./Components/Facility/Facility.component.js";
 
 const App: FC = () => {
   return (
---
+++ src/Components/Facility/Facility.types.ts
@@ -0,0 +1,5 @@
+ export interface FacilityCard {
+  img: string;
+  title: string;
+  desc: string;
+}
