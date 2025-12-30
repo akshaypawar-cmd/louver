5dfe2e65b24bdf01d7f7da3c35e1ebb007901438
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2025-12-30 11:34:23 +0530

    resolved all issues

--- src/Components/Competition/Competition.tsx
+++ src/Components/Competition/Competition.tsx
@@ -5,42 +5,35 @@ import imgCom from "@assets/Competition.jpg";
 
 const Competition: FC = () => {
 
+const commonButtonClass ="px-4 py-2 text-xs md:p-2 rounded-full font-medium border border-gray-500";
+
   return (
     <section className="max-w-7xl mx-auto px-4 py-16">
       <div className="grid grid-cols-1 md:grid-cols-12 gap-10 items-center">
         <div className="md:col-span-5 flex flex-col gap-6">
           <div className="flex flex-wrap gap-3">
-            <button className="px-4 py-1.5 text-xs md:p-2 rounded-full font-medium border-gray-500 border bg-sky-200 text-black">
+            <button className={`${commonButtonClass} bg-sky-200 text-black`}>
               Competition
             </button>
-            <button className="px-4 py-1.5 text-xs rounded-full md:p-2 font-medium border-gray-500 border">
-              Training
-            </button>
-            <button className="px-4 py-1.5 text-xs rounded-full md:p-2 font-medium border-gray-500 border">
-              Friendly Match
-            </button>
+
+            <button className={commonButtonClass}>Training</button>
+
+            <button className={commonButtonClass}>Friendly Match</button>
           </div>
 
-          <h2 className="text-3xl md:text-5xl font-light leading-tight">
-            Experience the best
-            <span className="block">in Courts, Fields, </span>
-            <span className="block">and More</span>
+          <h2 className="text-3xl md:text-5xl font-light leading-tight max-w-xl">
+            Experience the best in Courts, Fields, and More
           </h2>
 
           <p className="text-gray-600 max-w-md">
-            Our state-of-art sports facilities offer a variety of courts
-            <span className="md:block">
-              fields, including tennis, basketball and football,
-            </span>
-            <span className="md:block">
-              and designed to cater to both casual players and
-            </span>
-            <span>competitive athletes.</span>
+            Our state-of-art sports facilities offer a variety of courts,
+            fields, including tennis, basketball and football, and designed to
+            cater to both casual players and competitive athletes.
           </p>
 
           <button className="bg-gray-900 text-white px-5 py-2 rounded-full w-fit flex items-center gap-2 font-normal">
             Book Now
-            <ArrowUpRight className="size-5"/>
+            <ArrowUpRight className="size-5" />
           </button>
         </div>
 
