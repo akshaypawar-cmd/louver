e8110dd119c44fdb4dc572284032c3d0f6fd4190
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2025-12-31 15:23:33 +0530

    all componet update

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       "@assets":path.resolve(__dirname,"./src/assets"),
       "@components":path.resolve(__dirname,"./src/components"),
       "@mockData":path.resolve(__dirname,"./src/mockData")
+
     },
   },
 })
