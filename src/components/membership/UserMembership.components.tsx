d23c738713c9423a62ac21b6271a8fe3cbf43cd4
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2025-12-31 11:19:33 +0530

    resolved all issues

--- src/components/membership/UserMembership.components.tsx
+++ src/components/membership/UserMembership.components.tsx
@@ -7,38 +7,38 @@ const UserMembership:FC = () => {
 
   return (
  <>
-      <section className="bg-black h-1/3 mb-10 m-4 flex items-center justify-center rounded-2xl p-4">
+      <section className="bg-black h-1/3 mb-10 m-4 flex items-center justify-center rounded-2xl p-2">
         <div className="max-w-7xl w-full flex flex-col md:flex-row items-center gap-8">
           <div className="w-full md:w-1/2">
             <img
               src={membershipImg}
               alt="Membership"
-              className="w-full h-86 object-cover rounded-3xl"
+              className="w-full h-80 object-cover rounded-3xl"
             />
           </div>
 
-          <div className="w-full md:w-full">
+          <div className="w-full">
             <div className="flex gap-4 mb-6">
-              <button className="px-6 p-3 md:py-4 border border-white text-white rounded-full text-sm font-medium">
+              <button className="px-5 py-2 md:py-4 border border-white text-white rounded-full text-xm font-medium">
                 Membership
               </button>
-              <button className="px-6 py-3 bg-gray-200 text-black rounded-full text-sm font-medium">
+              <button className="px-5 py-3 bg-gray-200 text-black rounded-full text-sm font-medium">
                 Newsletter
               </button>
             </div>
 
             <div className="mb-6 w-full">
-              <h2 className="text-xl md:text-4xl line-clamp-4 font-bold text-white mb-2">
+              <h2 className="text-xl md:text-4xl font-bold text-white mb-2">
                 Get the Last Updates,<span className="block md:inline">Special Offers, </span> <span className="md:block"> and Exclusive Event
                 Invitations! </span> 
-              </h2>
+              </h2> 
             </div>
 
             <div className="relative w-full md:w-1/2">
               <input
                 type="email"
                 placeholder="Enter your email"
-                className="w-full px-6 py-4 pr-14 rounded-full bg-white text-black placeholder-text-white outline-none"
+                className="w-full px-6 py-4 pr-14 rounded-full bg-gray-900 text-white outline-none"
               />
               <button
                 type="button"
