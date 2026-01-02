ae581f2be1afc2a9c4df80e9623c1eed62be0210
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2026-01-02 19:47:54 +0530

    update all code

--- src/components/gymDetailSection/UserInfo.component.tsx
+++ src/components/gymDetailSection/UserInfo.component.tsx
@@ -13,15 +13,15 @@ const UserInfo: FC = () => {
             <img
               src={girl}
               alt="Gym"
-              className="w-full h-72 object-cover shadow-2xl"
+              className="w-full h-96 object-cover shadow-2xl"
             />
           </div>
 
           <div className="w-full md:w-1/2 p-6 md:p-8 flex flex-col md:mr-10 justify-center order-1 md:order-2">
-            <h2 className="md:text-3xl text-4xl font-light text-gray-900 mb-2">
+            <h2 className="text-5xl font-light text-gray-900 mb-2">
               Get in Touch
             </h2>
-            <p className="text-gray-600 text-xs text-start mb-6">
+            <p className="text-gray-600 text-sm mb-6">
               Reach out and we'll get in touch within 24 hours.
             </p>
 
